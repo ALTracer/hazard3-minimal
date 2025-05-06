4099701f81bdfe863d292ed7167d3299c6388562
ALTracer <11005378+ALTracer@users.noreply.github.com>
2025-05-06 16:49:16 +0300

    Fix uart_mini baudrate

--- uart/main.c
+++ uart/main.c
@@ -7,7 +7,7 @@ void uart_setup(const int baudrate)
 	UART1->CSR = 0x1;
 	const uint32_t SystemCoreClock = 12000000;
 	/* Simple divider. Round up. */
-	uint16_t divval = (SystemCoreClock + baudrate/2) / baudrate;
+	uint16_t divval = (SystemCoreClock*(16/8) + baudrate/2) / baudrate;
 	UART1->DIV = divval;
 }
 
@@ -17,12 +17,13 @@ void uart_write(const char *buf, const size_t len)
 		while(UART1->CSR & UART_CSR_BUSY);
 		UART1->TX = buf[i];
 	}
+	while(UART1->CSR & UART_CSR_BUSY);
 }
 
 int main(int argc, char *argv[])
 {
 	uart_setup(115200);
-	const char msg[] = "Hello, World!\n";
+	const char msg[] = "Hello, World!\r\n";
 	uart_write(msg, sizeof(msg));
 
 	while(1) {
